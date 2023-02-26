d73cf1acd423ff5a1230ae81052de1c69b363adc
Fausto <faustocamp545@gmail.com>
2023-02-26 08:58:50 -0500

    CRUD Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Employee } from './model/employee';
-import { EmployeeService } from './service/employee.service';
+import {HttpErrorResponse} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from './model/employee';
+import {EmployeeService} from './service/employee.service';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,29 @@ import { EmployeeService } from './service/employee.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  
-  public employees: Employee[] = [];
 
-  constructor(private employeeService: EmployeeService) { }
+  public employees: Employee[] = [];
+  public employeeTemplate: Employee;
+  public modalMode: string;
+
+  constructor(private employeeService: EmployeeService) {
+    this.employeeTemplate = this.cleanEmployee();
+    this.modalMode = '';
+  }
 
   ngOnInit(): void {
     this.getEmployees();
+  }
+
+  public cleanEmployee(): any {
+    return {
+      id: null,
+      name: '',
+      email: '',
+      phone: '',
+      jobTitle: '',
+      imageUrl: ''
+    };
   }
 
   public getEmployees(): void {
@@ -27,5 +44,68 @@ export class AppComponent implements OnInit {
         alert(error.message);
       }
     );
+  }
+
+  public onOpenModal(mode: string, employee?: Employee): void {
+    this.modalMode = mode;
+    this.employeeTemplate = this.cleanEmployee();
+    const container = document.getElementById('main_container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    switch (mode) {
+      case 'add':
+        button.setAttribute('data-target', '#employeeModal');
+        break;
+      case 'edit':
+        this.employeeTemplate = {...employee!};
+        button.setAttribute('data-target', '#employeeModal');
+        break;
+      case 'delete':
+        this.employeeTemplate = {...employee!};
+        button.setAttribute('data-target', '#deleteEmployeeModal');
+        break;
+    }
+    container!.appendChild(button);
+    button.click();
+  }
+
+  public onAddEmployee(addForm: NgForm): void {
+    document.getElementById('employee-form')!.click();
+    if (this.modalMode === 'add') {
+      this.employeeService.addEmployee(addForm.value).subscribe(
+        (response) => {
+          console.log(response);
+          this.getEmployees();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message)
+        }
+      )
+    }
+
+    if (this.modalMode === 'edit') {
+      this.employeeService.updateEmployee(this.employeeTemplate).subscribe(
+        (response) => {
+          console.log(response);
+          this.getEmployees();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message)
+        }
+      )
+    }
+  }
+
+  public onDeleteEmployee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response) => {
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
   }
 }
